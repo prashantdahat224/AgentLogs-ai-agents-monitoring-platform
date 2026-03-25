234198495134362edd2f54e2f2b2044f553eb907
prashant dahat <prashantdahat224@gmail.com>
2026-03-25 07:06:51 -0700

    Update code with latest changes -7

--- backend/main.py
+++ backend/main.py
@@ -128,5 +128,9 @@ def logs(x_api_key: str = Header(...)):
     }
 
 
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
